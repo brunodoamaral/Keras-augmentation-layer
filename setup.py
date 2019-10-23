0942a930f90b6b5f5e90a7e86a5045087cbec369
Bruno G. do Amaral <bruno.do.amaral [at] gmail.com>
2019-10-23 09:55:27 -0300

    Added setup.py and .gitignore

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='keras_augm_layer',
+      version='0.1',
+      description='Keras implementation of layer which performs augmentations of images using GPU.',
+      url='https://github.com/ZFTurbo/Keras-augmentation-layer',
+      author='ZFTurbo',
+      author_email='turbo@ippm.ru',
+      license='MIT',
+      packages=['keras_augm_layer'],
+      zip_safe=False)
